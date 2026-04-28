ee299cd5c02cbe610d27ba3e4dfb001d5a02e2f0
Jordan Dutel <162712550+jdutel42@users.noreply.github.com>
2026-04-28 13:21:22 +0200

    Update Slurm job script for Cell Ranger Flow

--- Slurm_job_nextflow.sh
+++ Slurm_job_nextflow.sh
@@ -7,6 +7,8 @@
 #SBATCH --output=Log/nextflow_controller_%j.out
 #SBATCH --error=Log/nextflow_controller_%j.err
 
-cd /labos/UGM/home/dutel/CellRanger_Flow
-source /chemin/vers/votre/env_nextflow/bin/activate
+cd /labos/UGM/home/dutel/cellranger-flow
+module load devel/python/Miniconda3-py39_4.10.3
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate nextflow_env
 nextflow run main.nf -c assets/nextflow.config -params-file assets/params.json -profile slurm -resume
